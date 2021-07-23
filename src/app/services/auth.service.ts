8409484d282eb1f5369d86ba2e459dc9e3c5f588
haritsfai713 <77919519+haritsfai713@users.noreply.github.com>
2021-07-23 10:27:19 +0700

    ngubah auth servce

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,17 +7,17 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  private _url = "http://localhost:3000/api"
+  private _url = "http://localhost:3000/api/v1"
 
   constructor(private http: HttpClient, private rout: Router) {
 
   }
 
   registerUser(user:any) {
-    return this.http.post<any>(this._url + "/register",user);
+    return this.http.post<any>(this._url + "/users/register",user);
   }
   loginUser(user:any) {
-    return this.http.post<any>(this._url + "/login",user);
+    return this.http.post<any>(this._url + "/users/login",user);
   }
 
   loggedin() {
