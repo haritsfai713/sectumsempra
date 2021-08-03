b8762e8b9da08959fa207981349d3362a8226f38
haritsfai713 <77919519+haritsfai713@users.noreply.github.com>
2021-08-03 17:54:53 +0700

    memperbaiki gui connect

--- src/app/services/mavlink.service.ts
+++ src/app/services/mavlink.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-const mavlink = require('mavlink');
+// const mavlink = require('mavlink');
 
 @Injectable({
   providedIn: 'root'
@@ -8,45 +8,45 @@ export class MavlinkService {
 
   constructor() { }
 
-  loadMavlinkSerial(port: any) {
-    mavlink.on("ready", function () {
-      //parse incoming serial data
-      port.on("data", function (data: any) {
-       mavlink.parse(data);
-      });
+  // loadMavlinkSerial(port: any) {
+  //   mavlink.on("ready", function () {
+  //     //parse incoming serial data
+  //     port.on("data", function (data: any) {
+  //      mavlink.parse(data);
+  //     });
 
-      //listen for messages
-     mavlink.on("message", function (message: any) {
-        console.log(message);
-      });
+  //     //listen for messages
+  //    mavlink.on("message", function (message: any) {
+  //       console.log(message);
+  //     });
 
-      port.on("message", () => {
-       mavlink.on("HEARTBEAT", function (message: any , fields: any) {
-          console.log(fields);
-        });
-      });
+  //     port.on("message", () => {
+  //      mavlink.on("HEARTBEAT", function (message: any , fields: any) {
+  //         console.log(fields);
+  //       });
+  //     });
 
-      // myMAV.createMessage(
-      //   "PARAM_REQUEST_LIST",
-      //   {
-      //     //Membuat messagenya terlebih dahulu
-      //     target_system: 1,
-      //     target_component: 1,
-      //   },
-      //   function (message) {
-      //     console.log("Requesting all parameters to PX4...");
-      //     //Gunakan mavlink v1 (karena meliputi array of char)
-      //     use_v1 = true;
-      //     setTimeout(() => {
-      //       //Mengirim PARAM_REQUEST_LIST ke PX4
-      //       port.write(message.buffer);
-      //     }, 1000);
-      //     setTimeout(() => {
-      //       //Timeout untuk mematikan parser mavlink v1
-      //       use_v1 = false;
-      //     }, 2000);
-      //   }
-      // );
-    })
-  }
+  //     // myMAV.createMessage(
+  //     //   "PARAM_REQUEST_LIST",
+  //     //   {
+  //     //     //Membuat messagenya terlebih dahulu
+  //     //     target_system: 1,
+  //     //     target_component: 1,
+  //     //   },
+  //     //   function (message) {
+  //     //     console.log("Requesting all parameters to PX4...");
+  //     //     //Gunakan mavlink v1 (karena meliputi array of char)
+  //     //     use_v1 = true;
+  //     //     setTimeout(() => {
+  //     //       //Mengirim PARAM_REQUEST_LIST ke PX4
+  //     //       port.write(message.buffer);
+  //     //     }, 1000);
+  //     //     setTimeout(() => {
+  //     //       //Timeout untuk mematikan parser mavlink v1
+  //     //       use_v1 = false;
+  //     //     }, 2000);
+  //     //   }
+  //     // );
+  //   })
+  // }
 }
